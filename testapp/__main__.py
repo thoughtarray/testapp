f3c363f00b894a928651a3340653205d1dad04ef
Kennedy C. Brown <kennedy.c.brown@gmail.com>
2016-08-11 00:50:45 -0700

    Working static-dependency mode (#3)

--- testapp/__main__.py
+++ testapp/__main__.py
@@ -1,6 +1,8 @@
 import argparse
+import json
 import logging
 import re
+from urlparse import urlparse
 
 import flask
 import requests
@@ -12,12 +14,105 @@ app = flask.Flask(NAME)
 
 @app.route('/')
 def index():
-    http_code, http_body = app.config['S']['return']
+    mode = app.config['S']['mode']
+
+    if mode == 'stand-alone':
+        return _stand_alone(app.config['S'], flask.request)
+
+    if mode == 'static-dependency':
+        return _static_dependency(app.config['S'], flask.request)
+
+def _stand_alone(settings, request):
+    http_code, http_body = settings['return']
+
+    # stand-alone mode may recieve chain-script requests (most likely the end)
+    is_chain_request = 'chain' in request.args
+
+    if is_chain_request:
+        return json.dumps(http_body), http_code, None
+
     return http_body, http_code, None
+
+def _static_dependency(settings, request):
+    is_single_dep = len(settings['static_deps']) == 1
+    is_chain_request = 'chain' in request.args
+    chain = []
+
+    if is_chain_request:
+        raw_chain = request.args.get('chain', None)
+        chain = raw_chain.split(',') if raw_chain else []
+        if len(chain) and chain[0] == settings['name']: chain = chain[1:]
+
+    http_code, http_body = settings['return']
+
+    # Same action for single or multi deps with chain-script request
+    if is_chain_request:
+        # If chain is done, return
+        if not len(chain):
+            return json.dumps([http_body]), http_code, None
+
+        next_dep = chain[0]
+        # Chain link must exist in dependencies
+        if next_dep not in settings['static_deps']:
+            return json.dumps('{} does not have dependency {}' \
+                .format(settings['name'], next_dep)), 404, None
+
+        url = normalize_url(settings['static_deps'][next_dep])
+        query_params = { 'chain': ','.join(chain) }
+
+        try:
+            r = requests.get(url, params=query_params, headers=settings['headers'])
+        except requests.ConnectionError as e:
+            loc = urlparse(url).netloc
+            body = "Error contacting {}".format(loc)
+            return json.dumps(body), 404, None
+
+        try:
+            r_content = json.loads(r.content)
+        except ValueError:
+            # Content isn't JSON; must be a plain string; turn into arr
+            r_content = [r.content]
+
+        # Combine last instance's return with this instance's return
+        r_content = [http_body] + r_content if isinstance(r_content, list) \
+            else [http_body, r_content]
+
+        return json.dumps(r_content), http_code, None
+
+    # Single dep doesn't require chain-script request
+    if is_single_dep:
+        url = normalize_url(settings['static_deps'].items()[0][1])
+        r = requests.get(url, headers=settings['headers'])
+        return r.content, r.status_code, None
+
+    # Multi dep requires chain-script request
+    return '{} has multiple dependencies; chain-script request required' \
+        .format(settings['name']), 400, None
 
 @app.route('/meta/health')
 def health():
     return '[]'
+
+# ---
+
+def normalize_url(url):
+    url = url.strip()
+    match = re.match(r'^(http:\/\/|https:\/\/)', url)
+
+    if not match:
+        return 'http://{}'.format(url)
+
+    return url
+
+def _kv_to_tup(s):
+    match = re.match(r'(.*)=(.*)', s)
+    if not match:
+        raise ValueError(s)
+
+    return match.group(1).strip(), match.group(2).strip()
+
+def _kv_arr_to_dict(a):
+    return dict([ _kv_to_tup(i) for i in a ])
 
 if __name__ == '__main__':
     global app_name
@@ -33,16 +128,25 @@ if __name__ == '__main__':
         help='Name of instance; default: testapp')
     parser.add_argument('-p', '--port', default=80,
         help='Port of instance; default: 80')
-    parser.add_argument('-r', '--return', default='200:' + NAME,
-        metavar='CODE:MESSAGE',
+    parser.add_argument('-r', '--return', metavar='CODE:MESSAGE',
         help='Success return; default: 200:NameOfInstance')
+    parser.add_argument('-s', '--static', action='append', default=[],
+        dest='static_deps', help='Other faux app to call; format: ' \
+            'host:port[/path]')
     parser.add_argument('-H', '--header', action='append', default=[],
         dest='headers', help='HTTP header (additive property); may use '
             '"{}" in dynamic-dependency mode')
 
     s = vars(parser.parse_args())
 
+    # Computed defaults
+    s['return'] = s['return'] or "200:{}".format(s['name'])
+
     # Validate args
+
+    s['mode'] = 'stand-alone'
+
+    # port
     try:
         s['port'] = int(s['port'])
         if not 1 <= s['port'] <= 65535:
@@ -51,6 +155,7 @@ if __name__ == '__main__':
         print 'Port must be an integer between 1-65535'
         exit(3)
 
+    # return
     match = re.match(r'(\d{3}):(.*)', s['return'])
     if not match:
         print 'Return must be formatted as CODE:MESSAGE where CODE is a ' \
@@ -60,16 +165,23 @@ if __name__ == '__main__':
     http_code, http_body = match.group(1).strip(), match.group(2).strip()
     s['return'] = http_code, http_body
 
-    headers = []
-    for h in s['headers']:
-        match = re.match(r'(.*)=(.*)', h)
-        if not match:
-            print 'Header "{}" must be formatted as KEY=VALUE where KEY and ' \
-                'VALUE are any arbitrary strings'.format(h)
-            exit(3)
-        h = match.group(1).strip(), match.group(2).strip()
-        headers.append(h)
-    s['headers'] = headers
+    # static
+    try:
+        s['static_deps'] = _kv_arr_to_dict(s['static_deps'])
+    except ValueError as e:
+        print 'Static (dependency) "{}" must be formatted as KEY=VALUE where ' \
+        'KEY and VALUE are any arbitrary strings'.format(e.value)
+        exit(3)
+
+    try:
+        s['headers'] = _kv_arr_to_dict(s['headers'])
+    except ValueError as e:
+        print 'Header "{}" must be formatted as KEY=VALUE where KEY and ' \
+            'VALUE are any arbitrary strings'.format(e.value)
+        exit(3)
+
+    if len(s['static_deps']):
+        s['mode'] = 'static-dependency'
 
     # Run app
     app.config['S'] = s
